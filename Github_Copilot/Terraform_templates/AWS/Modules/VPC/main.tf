735d3b9eeb8ce69f94301b276f632370cd70d44d
Saravana011 <161407668+Saravana011@users.noreply.github.com>
2025-08-04 17:19:39 +0530

    adding modules

---
+++ Github_Copilot/Terraform_templates/AWS/Modules/VPC/main.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.az_public
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.az_private
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
