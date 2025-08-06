c28753ac951dae905a87e8f0bee8c3f6eddbb27b
Avinash1996e <avinashsivakumaran@outlook.com>
2025-08-06 17:08:00 +0530

    Update output.tf

--- Github_Copilot/Terraform_templates/AWS_VPC/output.tf
+++ Github_Copilot/Terraform_templates/AWS_VPC/output.tf
@@ -1,1 +1,30 @@
+output "vpc_id" {
+description = "The ID of the VPC"
+value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+description = "The ID of the public subnet"
+value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+description = "The ID of the private subnet"
+value = aws_subnet.private.id
+}
+
+output "internet_gateway_id" {
+description = "The ID of the Internet Gateway"
+value = aws_internet_gateway.igw.id
+}
+
+output "public_route_table_id" {
+description = "The ID of the public route table"
+value = aws_route_table.public.id
+}
+
+output "security_group_id" {
+description = "The ID of the security group"
+value = aws_security_group.web_sg.id
+}
 
