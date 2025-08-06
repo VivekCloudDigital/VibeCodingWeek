ce22eb4b43f0c89dab54673d8edab90851fb73d0
Saravana011 <161407668+Saravana011@users.noreply.github.com>
2025-08-06 11:31:27 +0530

    adding demo aws for test

---
+++ Github_Copilot/Terraform_templates/demo_aws/variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default     = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 (us-east-1)
+  description = "AMI ID for EC2 instance"
+}
